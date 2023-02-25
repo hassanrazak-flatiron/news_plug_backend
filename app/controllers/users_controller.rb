4024de4129e4724a95913abb42836f0538123962
Hassan Razak <hassanrazak1991@gmail.com>
2023-02-25 09:01:29 -0500

    sign up functionality created.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
-    has_secure_password
+
+
+    def create
+        new_user = User.create!(user_params)
+        session[:user_id] = new_user.id
+        render json: new_user, status: :created
+    end
+
+
+
+
+    private
+
+    def user_params
+        params.permit(:first_name, :last_name,:email,:password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # get '/hello', to: 'application#hello_world'
-
+# returns all the headlines from the Top Headlines table
   get '/headlines', to: 'top_headlines#index'
+
+  ### signup ###
+  post '/signup', to: 'users#create'
 end
